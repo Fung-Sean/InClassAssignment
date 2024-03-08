778f594f896d9f308916732d159af36bc1b0d7cb
Fung-Sean <98567885+Fung-Sean@users.noreply.github.com>
2024-03-07 19:29:31 -0500

    Adding to data class

--- app/src/main/java/com/example/retrofit/MovieResponse.kt
+++ app/src/main/java/com/example/retrofit/MovieResponse.kt
@@ -3,11 +3,12 @@ package com.example.retrofit
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class MovieResponse(
 
     val Title: String,
     val Year: String,
-    val Poster:
-// Add other properties as needed
+    @Json(name = "Poster") val Poster: String,
+
 
 )
